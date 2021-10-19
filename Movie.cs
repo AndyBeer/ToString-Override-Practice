e086bf84272a056233a8a74fd857fbdd1f87be0e
Andy Beer <AndrewBeer@RocketMortgage.com>
2021-10-19 13:09:45 -0400

    First commit for ToString Practice

---
+++ Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movie_List
+{
+    class Movie
+    {
+        //These are properties title and category
+        public string Title { get; set; }
+        public string Category { get; set; }
+        //This is a constructor for the properties
+        public Movie(string Title, string Category)
+        {
+            this.Title = Title;
+            this.Category = Category;
+        }
+        public override string ToString()
+        {
+            //return base.ToString();
+            string output = $"Title: {Title}\n";
+            output += $"Category: {Category}\n";
+
+            return output;
+        }
+    }
+
+}
